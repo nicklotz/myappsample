2e699b731a8346cb9f27e457ec2e6ae5ec550785
nicklotz <nlotzextemp@gmail.com>
2025-02-07 11:13:34 -0600

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return 'Hello! Welcome to My Web App Version 3!'
+    return 'Hello! Welcome to My Web App Version 4!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
